34a0e74295098ead9061c0ff47f8c67a507de90b
Caroline Klug <clinek11@gmail.com>
2025-01-29 16:03:21 -0500

    Solidifies category styling

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var petNames = ['Rosie','Poppy','Noodles','Olive', 'Frannie'];
+var petNames = ['Rosie','Poppy','Twiggy','Olive', 'Ginkgo'];
 
 function generatePetName(){
     var randomIndex = Math.floor(Math.random() * petNames.length);
     var randomName = petNames[randomIndex];
-    document.getElementById("random-name").textContent= ("Welcome " + randomName);
+
+    document.getElementById("button").style.display="none";
+    document.getElementById("random-name").innerHTML= (randomName + "!");
+    document.getElementById("another-name").style.display="block";
 }
