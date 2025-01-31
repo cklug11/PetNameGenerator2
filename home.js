ed3e2a4998cbba7f445fd1e31182bf59824f58c9
Caroline Klug <clinek11@gmail.com>
2025-01-31 17:21:39 -0500

    Changes size

--- home.js
+++ home.js
@@ -3,7 +3,11 @@ function updateTextOnResize() {
     var textDiv = document.getElementById("firstText");
     
     if (window.innerWidth <= 430) {
-      textDiv.innerHTML = "Congratulations <br> On Your<br> New Pet!";
-    } 
+      textDiv.innerHTML = "Congratulations<br>On Your<br> New Pet!";
+    }  else {
+    textDiv.innerHTML = "Congratulations On Your New Pet!";  
+    }
   }
-window.addEventListener("resize", updateTextOnResize());
+updateTextOnResize();
+
+window.addEventListener("resize", updateTextOnResize)
