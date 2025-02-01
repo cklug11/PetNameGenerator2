a3cc8f3a1a25cef59b205760bd913fd97d35f445
Caroline Klug <clinek11@gmail.com>
2025-02-01 15:41:38 -0500

    Removes more unneeded files

--- JS/home.js
+++
@@ -1,9 +0,0 @@
-//For homepage
-function updateTextOnResize() {
-    var textDiv = document.getElementById("firstText");
-    
-    if (window.innerWidth <= 430) {
-      textDiv.innerHTML = "Congratulations <br> On Your<br> New Pet!";
-    } 
-  }
-window.addEventListener("resize", updateTextOnResize());
